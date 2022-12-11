1ab74142395d4e2034845611a9e009ce9ff31268
Артем Канаев <kanaev.aiu@phystech.edu>
2022-12-11 17:59:21 +0300

    Nothing

--- AntiYoy/Source.cpp
+++ AntiYoy/Source.cpp
@@ -6,24 +6,24 @@
 
 int main() {
 	MainCore Engine = MainCore();
-	while (Engine.gamestatus) {
-		for (unsigned short int i = 0; i < 2; i++) {
-			Engine.players[i]->movestatus = true;
-			while (Engine.players[i]->movestatus) {
-				/*Engine.GraphicsCore.render();
-				string a = Engine.GraphicsCore.events();
-				if (a == 'move') {
-					Engine.moveprocess();
-				}
-				if (a == 'buy') {
-					Engine.buyprocess();
-				}
-				if (a == 'end') {
-					Engine.players[i].movestatus == false;
-				}*/
-			}
-		}
-		/*Engine.proccessing();*/
-	}
+	//while (Engine.gamestatus) {
+	//	for (unsigned short int i = 0; i < 2; i++) {
+	//		Engine.players[i]->movestatus = true;
+	//		while (Engine.players[i]->movestatus) {
+	//			/*Engine.GraphicsCore.render();
+	//			string a = Engine.GraphicsCore.events();
+	//			if (a == 'move') {
+	//				Engine.moveprocess();
+	//			}
+	//			if (a == 'buy') {
+	//				Engine.buyprocess();
+	//			}
+	//			if (a == 'end') {
+	//				Engine.players[i].movestatus == false;
+	//			}*/
+	//		}
+	//	}
+	//	/*Engine.proccessing();*/
+	//}
 	return 0;
 }
